83b9284b6037d6cc5ac973c0f26419e20f045c03
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-08-27 08:33:47 -0700

    Added FeatredLink and FeaturedLinkDetails components to the list of exported components.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import "./styles/styles.scss";
 export * from "./components/Breadcrumbs";
 export * from "./components/Card";
 export * from "./components/Chip";
+export * from "./components/FeaturedLink";
+export * from "./components/FeaturedLinkDetails";
 export * from "./components/FeaturedLinkListItem";
 export * from "./components/Header";
 export * from "./components/HelloWorld";
