0b3bcee54aa9f75fecc91717736f1d380a5d0e7d
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-11 12:25:57 -0700

    Updated Card styling and behaviors. Also incorporated links.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,38 @@
-import Typography from '@mui/material/Typography';
 import MaterialCard from '@mui/material/Card';
 import { CardMediaProps } from '@mui/material/CardMedia';
 import CardMedia from '@mui/material/CardMedia';
 import testImage from '../../nasaTest.jpeg';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
+import { Typography } from 'components/Typography';
+import { PrimaryButton } from 'components/PrimaryButton';
+import { Link } from 'react-router-dom';
 
 export type CardProps = {
-  /** Title to display in the card */
-  title: string,
   /** Content to display in the card */
-  content: string,
+  description?: string;
+  /** Height of the card */
+  height: number;
   /** Image to display in the card */
-  image: CardMediaProps['image']
+  image: CardMediaProps['image'];
+  /** Width of the card **/
+  maxWidth: number;
+  /** Title to display in the card */
+  title: string;
+  /** Link to go to when card or button is clicked */
+  url: string;
+  /** Width of the card **/
+  width: number;
 }
 
 export const Card = ({
-  title = '',
-  content = '',
-  image = testImage
+  description = '',
+  height = 480,
+  image = testImage,
+  maxWidth = 345,
+  title,
+  url,
+  width = 312,
 }: CardProps) => {
   const [isCardHovered, setIsCardHovered] = useState(false);
 
@@ -31,65 +45,67 @@ export const Card = ({
   }
 
   return (
-    <MaterialCard
-      onMouseOver={onCardMouseOver} 
-      onMouseOut={onCardMouseOut} 
-      sx={{ 
-        maxWidth: 345, 
-        boxShadow:'none',
-        ':hover': {
-          cursor: 'pointer',
-        },
-        ':focus': {
-          border: '1px dotted',
-        }
-      }}
-    >
-      <Box sx={{ position: 'relative' }}>
-        <CardMedia
-          component="img"
-          height="480"
-          width="312"
-          image={image}
-          sx={{
-            transform: isCardHovered? 'scale(1.25)' : 'scale(1)',
-            transition: 'all .2s ease',
-            verticalAlign: 'middle'
-          }}
-        />
-        <Box
-          sx={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            height: '50%',
-            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))',
-            color: 'white',
-            padding: '20px 20px',
-          }}
-        />
-        <Box
-          sx={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            color: 'white',
-            padding: '20px 20px',
-          }}
-        >
-          <Typography variant="h5" component="span">{title} {/*<Badge color="secondary" badgeContent=" "><EastIcon/></Badge>*/}</Typography>
-          <Typography variant="body2">{content}</Typography>
+    <Link to={url}>
+      <MaterialCard
+        onMouseOver={onCardMouseOver} 
+        onMouseOut={onCardMouseOut} 
+        sx={{ 
+          minWidth: width,
+          maxWidth, 
+          boxShadow:'none',
+          ':hover': {
+            cursor: 'pointer',
+          },
+          ':focus': {
+            border: '1px dotted',
+          }
+        }}
+      >
+        <Box sx={{ position: 'relative' }}>
+          <CardMedia
+            component="img"
+            height={height}
+            width={width}
+            image={image}
+            sx={{
+              transform: isCardHovered? 'scale(1.25)' : 'scale(1)',
+              transition: 'all .2s ease',
+              verticalAlign: 'middle'
+            }}
+          />
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              width,
+              height: '50%',
+              background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))',
+              color: 'white',
+              //padding: '20px 20px',
+            }}
+          />
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              color: 'white',
+              padding: '20px 20px',
+            }}
+          >
+            <PrimaryButton label={title} size="16"></PrimaryButton>
+            <Typography variant="body2" weight="regular">{description}</Typography>
+          </Box>
+          <Box
+            sx={{
+              position: 'relative', top: '-10px', zIndex: '3'
+            }}
+          >
+          </Box>
         </Box>
-        <Box
-          sx={{
-            position: 'relative', top: '-10px', zIndex: '3'
-          }}
-        >
-        </Box>
-      </Box>
-    </MaterialCard>
+      </MaterialCard>
+    </Link>
   );
 }
 
