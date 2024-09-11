e9926a7acd72cb6d74f72537a99b700077b98d31
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-11 12:22:47 -0700

    Added ability to specify if icon in Primary Button should point to the right for internal links or diagonally for external links.

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,16 +1,18 @@
 import { IconButton as MuiIconButton, Stack as MuiStack } from '@mui/material';
 import { Typography } from 'components/Typography';
-import { IconArrowRight } from 'components/Icons';
+import { IconArrowDiagonal, IconArrowRight } from 'components/Icons';
 
 import "./PrimaryButton.scss"
 
 
 export type PrimaryButtonProps = {
+  iconType?: "internal" | "external"
   label: string;
   size?: "14" | "16" | "18" | "22" | "29" | "36";
 }
 
 export const PrimaryButton = ({
+  iconType = "internal",
   label,
   size = "16"
 }:PrimaryButtonProps) => {
@@ -23,7 +25,7 @@ export const PrimaryButton = ({
         <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"4px"}>
           <Typography variant={"h6"} weight={"semibold"}>{label}</Typography>
           <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-14"}>
-            <IconArrowRight />
+            { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
           </MuiIconButton>
         </MuiStack>
       </>
@@ -35,7 +37,7 @@ export const PrimaryButton = ({
           <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"4px"}>
             <Typography variant={"h5"} weight={"semibold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-16"}>
-              <IconArrowRight />
+              { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
         </>
@@ -46,7 +48,7 @@ export const PrimaryButton = ({
           <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"4px"}>
             <Typography variant={"h4"} weight={"semibold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-18"}>
-              <IconArrowRight />
+              { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
         </>
@@ -57,7 +59,7 @@ export const PrimaryButton = ({
           <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"8px"}>
             <Typography variant={"h3"} weight={"bold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-22"}>
-              <IconArrowRight />
+              { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
         </>
@@ -68,7 +70,7 @@ export const PrimaryButton = ({
           <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"8px"}>
             <Typography variant={"h2"} weight={"bold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-29"}>
-              <IconArrowRight />
+              { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
         </>
@@ -79,7 +81,7 @@ export const PrimaryButton = ({
           <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"8px"}>
             <Typography variant={"h1"} weight={"bold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-36"}>
-              <IconArrowRight />
+              { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
         </>
