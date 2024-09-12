7f58a225fd0e9c25ad19dcabcd52fd2120e92128
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-12 09:28:59 -0700

    Removed code that is no longer needed in QuickLinksBar Component in order to fix padding around component.

--- src/components/QuickLInksBar/QuickLinksBar.tsx
+++ src/components/QuickLInksBar/QuickLinksBar.tsx
@@ -1,4 +1,4 @@
-import { Box as MuiBox, Stack as MuiStack } from "@mui/material";
+import { Stack as MuiStack } from "@mui/material";
 import { Button } from "components/Button";
 import { PrimaryButton } from "components/PrimaryButton";
 import { Typography } from "components/Typography";
@@ -18,27 +18,25 @@ export const QuickLinksBar = ({
   title
 }:QuickLinksBarProps) => {
   return <>
-    <MuiBox className="pds-wds-quick-links-bar-root">
-      <MuiStack direction={{ xs: 'column', lg: 'row' }} alignItems={{xs: "flex-start", lg: "center"}} justifyContent={{xs: "flex-start", lg: "space-between"}} spacing={{ xs: "32px"}}>
-        <Typography variant="h2" weight="bold">{title}</Typography>
-        <MuiStack direction={{ xs: "column", lg: "row"}} spacing={{xs: "32px", lg: "16px"}}>
-          <MuiStack direction={{ xs: 'column', md: 'row' }} justifyContent={{ xs: "left",  lg:"flex-end"}} alignItems={{xs: "flex-start", lg: "center"}} spacing={"16px"}>
-            {
-              primaryLinks.map( (link, index) => {
-                return <Button variant="cta" key={index}>{link.label}</Button>
-              })
-            }
-          </MuiStack>
-          <MuiStack direction={{ xs: 'column', md: 'row' }} justifyContent={{ xs: "left",  lg:"flex-end"}} alignItems={{xs: "flex-start", md: "center", lg: "center"}} spacing={"16px"}>
-            {
-              secondaryLinks.map( (link, index) => {
-                return <PrimaryButton label={link.label} key={index}/>
-              })
-            }
-          </MuiStack>
+    <MuiStack direction={{ xs: 'column', lg: 'row' }} alignItems={{xs: "flex-start", lg: "center"}} justifyContent={{xs: "flex-start", lg: "space-between"}} spacing={{ xs: "32px"}}>
+      <Typography variant="h2" weight="bold">{title}</Typography>
+      <MuiStack direction={{ xs: "column", lg: "row"}} spacing={{xs: "32px", lg: "16px"}}>
+        <MuiStack direction={{ xs: 'column', md: 'row' }} justifyContent={{ xs: "left",  lg:"flex-end"}} alignItems={{xs: "flex-start", lg: "center"}} spacing={"16px"}>
+          {
+            primaryLinks.map( (link, index) => {
+              return <Button variant="cta" key={index}>{link.label}</Button>
+            })
+          }
+        </MuiStack>
+        <MuiStack direction={{ xs: 'column', md: 'row' }} justifyContent={{ xs: "left",  lg:"flex-end"}} alignItems={{xs: "flex-start", md: "center", lg: "center"}} spacing={"16px"}>
+          {
+            secondaryLinks.map( (link, index) => {
+              return <PrimaryButton label={link.label} key={index}/>
+            })
+          }
         </MuiStack>
       </MuiStack>
-    </MuiBox>
+    </MuiStack>
   </>
 }
 
