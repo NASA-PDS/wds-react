361d9297277248461d7cd47ebd4448087cbac7a4
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-12 05:27:33 -0700

    Updated structure of Primary Button.

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { IconButton as MuiIconButton, Stack as MuiStack } from '@mui/material';
+import { Box as MuiBox, IconButton as MuiIconButton, Stack as MuiStack } from '@mui/material';
 import { Typography } from 'components/Typography';
 import { IconArrowDiagonal, IconArrowRight } from 'components/Icons';
 
@@ -17,74 +17,89 @@ export const PrimaryButton = ({
   size = "16"
 }:PrimaryButtonProps) => {
 
+  const alignItems = "flex-end";
+  const justifyContent = "left";
+
   switch(size) {
 
     case "14":
 
       return <>
-        <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"4px"}>
-          <Typography variant={"h6"} weight={"semibold"}>{label}</Typography>
-          <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-14"}>
-            { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
-          </MuiIconButton>
-        </MuiStack>
+        <MuiBox>
+          <MuiStack direction={"row"} justifyContent={justifyContent} alignItems={alignItems} spacing={"4px"}>
+            <Typography variant={"h6"} weight={"semibold"}>{label}</Typography>
+            <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-14"}>
+              { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
+            </MuiIconButton>
+          </MuiStack>
+        </MuiBox>
       </>
 
     default:
     case "16":
 
       return <>
-          <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"4px"}>
+        <MuiBox>
+          <MuiStack direction={"row"} justifyContent={justifyContent} alignItems={alignItems} spacing={"4px"}>
             <Typography variant={"h5"} weight={"semibold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-16"}>
               { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
-        </>
+        </MuiBox>
+      </>
 
     case "18":
 
       return <>
-          <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"4px"}>
+        <MuiBox>
+          <MuiStack direction={"row"} justifyContent={justifyContent} alignItems={alignItems} spacing={"4px"}>
             <Typography variant={"h4"} weight={"semibold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-18"}>
               { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
-        </>
+        </MuiBox>
+      </>
 
     case "22":
 
       return <>
-          <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"8px"}>
+        <MuiBox>
+          <MuiStack direction={"row"} justifyContent={justifyContent} alignItems={alignItems} spacing={"8px"}>
             <Typography variant={"h3"} weight={"bold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-22"}>
               { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
-        </>
+        </MuiBox>
+      </>
 
     case "29":
 
       return <>
-          <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"8px"}>
+        <MuiBox>
+          <MuiStack direction={"row"} justifyContent={justifyContent} alignItems={alignItems} spacing={"8px"}>
             <Typography variant={"h2"} weight={"bold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-29"}>
               { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
-        </>
+        </MuiBox>
+      </>
 
     case "36":
 
       return <>
-          <MuiStack direction={"row"} justifyContent={"left"} alignItems={"center"} spacing={"8px"}>
+        <MuiBox>
+          <MuiStack direction={"row"} justifyContent={justifyContent} alignItems={alignItems} spacing={"8px"}>
             <Typography variant={"h1"} weight={"bold"} component={"span"}>{label}</Typography>
             <MuiIconButton aria-label="arrow" className={"pds-wds-primary-button pds-wds-primary-button-size-36"}>
               { iconType === "internal" ? <IconArrowRight/> : <IconArrowDiagonal /> }
             </MuiIconButton>
           </MuiStack>
-        </>
+        </MuiBox>
+      </>
 
   }
 
