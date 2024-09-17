bf17df319dc03c07ff4521d881f4156138499757
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-17 10:00:58 -0700

    Removed link behavior from MediaCard component as the link behavior should be handled one level above.

--- src/components/Cards/MediaCard/MediaCard.tsx
+++ src/components/Cards/MediaCard/MediaCard.tsx
@@ -5,7 +5,6 @@ import {
   CardMediaProps as MuiCardMediaProps,
   Stack as MuiStack 
 } from "@mui/material";
-import { Link } from "react-router-dom";
 import { Typography } from "components/Typography";
 import { IconArrowRight } from "components/Icons";
 import testImage from '../../../nasaTest.jpeg';
@@ -15,7 +14,6 @@ export type MediaCardProps = {
   image:MuiCardMediaProps['image'];
   imageDescription:string;
   title:string;
-  url:string;
 }
 
 export const MediaCard = ({
@@ -23,76 +21,74 @@ export const MediaCard = ({
   image = testImage,
   imageDescription,
   title,
-  url,
 }:MediaCardProps) => {
 
   return <>
-    <Link to={url}>
-      <MuiCard sx={{
-        height: "250px",
-        boxShadow: "none",
-        ':focus': {
-            border: '1px dotted',
-        },
-        borderRadius: "0px",
-        transition: "none"
+    <MuiCard sx={{
+      height: "250px",
+      boxShadow: "none",
+      ':focus': {
+          border: '1px dotted',
+      },
+      borderRadius: "0px",
+      transition: "none"
+    }}>
+      <MuiBox sx={{ 
+        width: "100%", 
+        height: "100%", 
+        position: 'relative', 
       }}>
-        <MuiBox sx={{ 
-          width: "100%", 
-          height: "100%", 
-          position: 'relative', 
-        }}>
-          <MuiCardMedia
-            component="img"
-            image={image}
-            width={"100%"}
-            height={"100%"}
-            sx={{ backgroundSize: "cover"}}
-            alt={imageDescription}
-          />
-          <MuiBox
-            sx={{
-              position: 'absolute',
-              bottom: 0,
-              left: 0,
-              width: "100%",
-              height: '50%',
-              background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))',
-              color: 'white',
-            }}
-          />
-          <MuiBox
-            sx={{
-              position: 'absolute',
-              bottom: 0,
-              left: 0,
-              color: 'white',
-              padding: '20px 20px',
-              gap: "12px",
-              display: "flex",
-              flexDirection: "column",
-              flexWrap: "wrap"
-            }}
-          >
-            <MuiStack direction={"row"} spacing={"4px"} alignItems={"flex-end"} width={"100%"} justifyContent={"space-between"}>
-                <Typography variant={"h5"} weight={"semibold"} component={"span"}>{title}</Typography>
-                <div style={{
+        <MuiCardMedia
+          component="img"
+          image={image}
+          width={"100%"}
+          height={"100%"}
+          sx={{ backgroundSize: "cover"}}
+          alt={imageDescription}
+        />
+        <MuiBox
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            height: '50%',
+            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))',
+            color: 'white',
+          }}
+        />
+        <MuiBox
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            color: 'white',
+            padding: '20px 20px',
+            gap: "12px",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap"
+          }}
+        >
+          <MuiStack direction={"row"} spacing={"4px"} alignItems={"flex-end"} width={"100%"} justifyContent={"space-between"}>
+              <Typography variant={"h5"} weight={"semibold"} component={"span"}>{title}</Typography>
+              <div style={{
                     display: "flex",
-                  backgroundColor: "#F64137",
-                  padding: "5px",
-                  width: "10px",
-                  height: "10px",
-                  borderRadius: "10px",
-                  color: "#FFFFFF"
-                }}>
-                  <IconArrowRight style={{ width: "10px", height: "10px" }}/>
-                </div>
-              </MuiStack>
-            { description && <Typography variant="body2" weight="regular">{description}</Typography> }
-          </MuiBox>
+                    backgroundColor: "#F64137",
+                    padding: "5px",
+                    width: "10px",
+                    height: "10px",
+                    borderRadius: "10px",
+                    color: "#FFFFFF"
+                  }}
+              >
+                <IconArrowRight style={{ width: "10px", height: "10px" }}/>
+              </div>
+            </MuiStack>
+          { description && <Typography variant="body2" weight="regular">{description}</Typography> }
         </MuiBox>
-      </MuiCard>
-    </Link>
+      </MuiBox>
+    </MuiCard>
   </>
 }
 
