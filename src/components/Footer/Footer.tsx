82cf150bc207957f607b16e1b9d0a0e08a8c572a
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-19 10:53:22 -0700

    Updated footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,33 @@
-import { Container as MuiContainer } from "@mui/material";
+import { 
+  Box as MuiBox, 
+  //Grid as MuiGrid,
+  Container as MuiContainer, 
+  Stack as MuiStack,
+  } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import nasaLogo from "../../assets/nasa.svg";
 
 import "./Footer.scss";
+import { Typography } from "components/Typography";
+import { Link } from "react-router-dom";
 
-export const Footer = () => {
+export type FooterLink = {
+  id: string;
+  label: string;
+  href: string;
+};
+
+export type FooterProps = {
+  primaryLinks:FooterLink[],
+  secondaryLinks:FooterLink[],
+  pageLastUpdated:string
+}
+
+export const Footer = ({
+  primaryLinks,
+  secondaryLinks,
+  pageLastUpdated
+}:FooterProps) => {
   return <>
     <footer>
       <MuiContainer
@@ -13,7 +38,69 @@ export const Footer = () => {
           maxWidth={"xl"}
           className="pds-wds-footer-container"
         >
-          Footer
+          <MuiStack direction={"column"} spacing={"24px"}>
+            <MuiBox>
+              <img src={nasaLogo} alt="The National Aeronautics and Space Administration (NASA) Logo" width={68} height={68} />
+            </MuiBox>
+            <Grid container>
+
+              <Grid xs={8} md={4} lg={3} sx={{mb: "40px"}}>
+                <MuiBox>
+                  <Typography variant="h3" weight="bold" sx={{my: "22px"}}>The National Aeronautics <br />and Space Administration</Typography>
+                  <Typography variant="body4" weight="regular" sx={{ my: "14px", color: "#F6F6F6"}}>NASA explores the unknown in air and space, innovates for the benefit of humanity, and inspires the world through discovery.</Typography>
+                  <p style={{margin: "0", paddingTop: "8px", paddingBottom: "8px"}}>
+                    <Link to={"https://www.nasa.gov/about/"} target="_blank" style={{color: "#D1D1D1"}}>
+                      <Typography variant="h6" weight="semibold" component="span">About NASA&apos;s Mission</Typography>
+                    </Link>
+                  </p>
+                </MuiBox>
+              </Grid>
+
+              {
+                <Grid xs={12} md={6} lg={6} mdOffset={1} >
+                  <MuiStack direction={"column"} sx={{ height: {xs: "325px", md: "225px"}, flexWrap: 'wrap', flexShrink: 1, flexGrow: 0 }}>
+                    {
+                      primaryLinks.map( (link:FooterLink, primaryLinkIndex) => {
+                        return <>
+                          <Link to={link.href} className={"pds-wds-footer-primary-link"} style={{color: "#D1D1D1", marginBottom: "19px"}} key={primaryLinkIndex}>
+                            <Typography variant="h6" weight="semibold" component="span">{link.label}</Typography>
+                          </Link>
+                        </>
+                      })
+                    }
+                  </MuiStack>
+                </Grid>
+
+              }
+
+            </Grid>
+            <MuiBox sx={{height: {xs: "150px", md: "unset"}, pt: "64px", mt: "64px", borderTop: "1px solid #2E2E32", marginBottom: "16px", paddingTop: "24px"}}>
+              {/* <MuiStack direction={{xs: "column", md: "row"}} spacing={"49px"}> */}
+              <MuiStack direction={{xs:"column", md: "row"}} spacing={{md: "49px"}} sx={{ height: {xs: "150px", md: "unset"}, flexWrap: 'wrap', flexShrink: 1, flexGrow: 0 }}>
+                {
+                  secondaryLinks.map( (link, index) => {
+                    return <>
+                      <Link to={link.href} className={"pds-wds-footer-secondary-link"} key={index}>
+                        <Typography variant="h6" weight="regular" component="span">{link.label}</Typography>
+                      </Link>
+                    </>
+                  })
+                }
+              </MuiStack>
+            </MuiBox>
+            <MuiBox>
+              <MuiStack direction={{xs:"column", md: "row"}} spacing={{md: "32px"}} sx={{flexWrap: 'wrap', flexShrink: 1, flexGrow: 0 }}>
+                <MuiStack direction={"row"} spacing={"4px"}>
+                  <Typography variant="h6" weight="regular" component="span" style={{color: "#959599", marginBottom: "8px"}}>Page Last Updated:</Typography>
+                  <Typography variant="h6" weight="semibold" component="span" style={{color: "#959599", marginBottom: "8px"}}>{pageLastUpdated}</Typography>
+                </MuiStack>
+                <MuiStack direction={"row"} spacing={"4px"}>
+                  <Typography variant="h6" weight="regular" component="span" style={{color: "#959599", marginBottom: "8px"}}>NASA Official:</Typography>
+                  <Typography variant="h6" weight="semibold" component="span" style={{color: "#959599", marginBottom: "8px"}}>Jordan Padams</Typography>
+                </MuiStack>
+              </MuiStack>
+            </MuiBox>
+          </MuiStack>
         </MuiContainer>
       </MuiContainer>
     </footer>
